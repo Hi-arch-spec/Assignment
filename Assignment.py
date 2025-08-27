40d6f9ecbfdd14615c4fb575af6cd17f1e6aa4db
Hi-arch-spec <hirdayeshraghav76@gmail.com>
2025-08-27 14:34:41 +0530

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,19 @@
+#Task 1
+
+x=int(input("Enter the first number:"))
+y=int(input("Enter the second number:"))
+
+add=x+y
+sub=x-y
+div=x/y
+multi=x*y
+print("Addition:",add)
+print("Subtraction:",sub)
+print("Multiplication:",multi)
+print("Division:",div)
+
+#Task 2
+
+a=input("Enter First Name :")
+b=input("Enter Last Name :")
+print("Hello",a,"",b,"!",",Welcome to the python Language")
